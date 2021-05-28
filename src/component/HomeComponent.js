f1162e11aab0b29d30f01443ade4367004503fc5
igiradiane <igiradiane@gmail.com>
2021-05-28 11:00:29 +0200

    UI of school management system

---
+++ src/component/HomeComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+ 
+
+const HomeComponent =()=>{
+    return (
+        <h1>hello</h1>
+    )
+}
+
+export default HomeComponent;
---
+++ src/view/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomeComponent from '../component/HomeComponent';
+
+
+
+const Home =()=>{
+    return(
+        <HomeComponent> </HomeComponent>
+    )
+}
+
+
+export default Home;
